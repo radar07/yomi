01d63c9adb0c3ad78fba02ccaf78bea77155e795
Pranav RK <pranavrk7@gmail.com>
2024-08-11 17:46:18 +0530

    feat: add templ, gin, and htmx

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/gin-gonic/gin"
+	"github.com/radar07/yomi/views"
 )
 
+func render(c *gin.Context, status int, template templ.Component) {
+	c.Status(status)
+}
+
 func main() {
-	fmt.Println("Hello, web!!")
+	router := gin.Default()
+	router.LoadHTMLGlob("views/*")
+	router.GET("/hello", func(ctx *gin.Context) {
+		ctx.Status(http.StatusOK)
+		component := views.Hello()
+		component.Render(ctx, ctx.Writer)
+	})
+
+	router.Run(":8080")
 }
